c840bd5a2c48c9c400fb48e67dc9a82117308dc1
Hesam Mousavi <=>
2023-04-22 18:22:48 +0100

    8th Commit

--- server/main.js
+++ server/main.js
@@ -2,7 +2,12 @@ const app = require("./server");
 
 // Set port number
 const PORT = process.env.PORT || 5000;
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "server", "build", "index.html"));
+  });
+}
 // Start the server to listen for request
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
