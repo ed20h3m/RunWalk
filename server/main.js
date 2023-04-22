8308d9059cc6d9fcdca478016ea6a25308eae9ca
Hesam Mousavi <=>
2023-04-22 21:44:41 +0100

    18th Commit

--- server/main.js
+++ server/main.js
@@ -9,6 +9,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("./build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "server", "build", "login"));
+    //
   });
 }
 // Start the server to listen for request
