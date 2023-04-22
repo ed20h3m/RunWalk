8439a2aef03d1c59c7b1e10d63cacadeaae515b9
Hesam Mousavi <=>
2023-04-22 21:00:23 +0100

    17th Commit

--- server/main.js
+++ server/main.js
@@ -6,9 +6,11 @@ const express = require("express");
 // Set port number
 const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "server", "build", "index.html"));
+  // app.use(express.static("build"));
+  app.use(express.static(path.resolve(__dirname, "./build")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./build", "index.html"));
   });
 }
 // Start the server to listen for request
