c1d02cceaa054ceafb2640cc37e23c850d2275cc
Hesam Mousavi <=>
2023-04-22 22:30:59 +0100

    20th Commit

--- server/main.js
+++ server/main.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "build")));
 
-  app.get("/", function (req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 
