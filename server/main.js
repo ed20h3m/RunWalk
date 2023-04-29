f221d7ea698c5a692b734aac477a7da2882f965e
Hesam Mousavi <=>
2023-04-30 00:24:55 +0100

    try7

--- server/main.js
+++ server/main.js
@@ -3,15 +3,15 @@ const path = require("path");
 require("dotenv").config();
 const express = require("express");
 
-app.use(express.static(path.join(__dirname, "/client/build/")));
+app.use(express.static(path.join(__dirname, "build")));
 // app.use(express.static("./client/build"));
 
 // Set port number
-console.log(path.resolve(__dirname, "../client/build", "index.html"));
+console.log(path.resolve(__dirname, "build", "index.html"));
 const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 // Start the server to listen for request
