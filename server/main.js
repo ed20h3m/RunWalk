1ee2b715a6a87de6eb9a59c80769d93c9eec19d1
Hesam Mousavi <=>
2023-04-29 01:40:09 +0100

    try4

--- server/main.js
+++ server/main.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, '/client/build/')));
 // Set port number
 const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
