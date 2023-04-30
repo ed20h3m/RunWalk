25f4928a1b343b5e1cdfac0895b3af8675eaed87
Hesam Mousavi <=>
2023-04-30 21:26:51 +0100

    try24

--- server/server.js
+++ server/server.js
@@ -71,7 +71,6 @@ server.use("/subscription", require("./routes/stripeSub"));
 // Create a transaction
 server.use("/transactions", require("./routes/Transactions"));
 
-//
 // Get sports centre info
 server.use("/info", require("./routes/Info"));
 
